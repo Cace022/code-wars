1e366cc90d096a3d581ba4a22c0fc0b257c66059
Cace022 <carlos.correa01@up.ac.pa>
2024-02-17 13:21:49 -0500

    Square(n)Sum done in pythong

---
+++ python/8kyu/ Square(n)Sum.py
@@ -0,0 +1,7 @@
+#Completa la función de suma cuadrada de modo que eleve al cuadrado cada número que se le pase y luego sume los resultados.
+
+#mi solucion 
+
+def square_sum(numbers):
+    return sum(x**2 for x in numbers)
+
