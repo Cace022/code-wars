9d22c1f26838cc8451e0cffb542b496c43db6715
Cace022 <carlos.correa01@up.ac.pa>
2024-02-18 12:59:08 -0500

    Convert a String to a Number in javascript

---
+++ javascript/8kyu/Convert-a-String-to-a-Number.js
@@ -0,0 +1,15 @@
+//Descripción
+//Necesitamos una función que pueda transformar una cadena en un número. ¿Qué formas conoces de lograrlo?
+
+//Nota: No te preocupes, todas las entradas serán cadenas, y cada cadena es una representación perfectamente válida de un número entero.
+
+//Ejemplos
+//"1234" --> 1234
+//"605"  --> 605
+//"1405" --> 1405
+//"-7" --> -7
+
+//mi solución
+const stringToNumber = function(str){
+    return Number (str)
+  }
